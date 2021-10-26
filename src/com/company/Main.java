55ff02cea55d6dd68856da48e11a114130a63ddc
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-10-26 17:01:39 -0300

    Segundo commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Teste");
+        System.out.println("Teste1");
+        System.out.println("Teste2");
     }
 }
